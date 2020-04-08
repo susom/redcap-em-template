666837d30c7371b6a2f94ee3454825c06050d604
Andrew Martin <andy123@stanford.edu>
2020-04-08 15:34:22 -0700

    initial template

---
+++ ClassNameHere.php
@@ -0,0 +1,30 @@
+<?php
+namespace Stanford\ClassNameHere;
+
+require_once "emLogger.php";
+
+class ClassNameHere extends \ExternalModules\AbstractExternalModule {
+
+    use emLogger;
+
+    public function __construct() {
+		parent::__construct();
+		// Other code to run when object is instantiated
+	}
+	
+	public function redcap_module_system_enable( $version ) {
+		
+	}
+
+	
+	public function redcap_module_project_enable( $version, $project_id ) {
+		
+	}
+
+	
+	public function redcap_module_save_configuration( $project_id ) {
+		
+	}
+
+	
+}
